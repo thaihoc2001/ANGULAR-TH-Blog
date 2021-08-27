04a8aaa07cd368fe618b8f4f57bb421007fb19c6
thaihoc2001 <thaihoc95x@gmail.com>
2021-08-27 14:09:40 +0700

    delete map

--- src/app/layout/home/component/contact/contact.component.ts
+++ src/app/layout/home/component/contact/contact.component.ts
@@ -11,17 +11,17 @@ export class ContactComponent implements OnInit {
   constructor() { }
   title = 'google-maps';
   ngOnInit(): void {
-    const loader = new Loader({
-      apiKey: 'AIzaSyA1CUd61iFAYf3Pn4nTMIhVchWX6V2r4B0'
-    });
-    loader.load().then(() => {
-      // @ts-ignore
-      // tslint:disable-next-line:no-unused-expression
-      new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 10.798618240242915, lng: 106.67010600067425},
-        zoom: 15,
-      });
-    });
+    // const loader = new Loader({
+    //   apiKey: 'AIzaSyA1CUd61iFAYf3Pn4nTMIhVchWX6V2r4B0'
+    // });
+    // loader.load().then(() => {
+    //   // @ts-ignore
+    //   // tslint:disable-next-line:no-unused-expression
+    //   new google.maps.Map(document.getElementById('map'), {
+    //     center: { lat: 10.798618240242915, lng: 106.67010600067425},
+    //     zoom: 15,
+    //   });
+    // });
   }
 
 }
